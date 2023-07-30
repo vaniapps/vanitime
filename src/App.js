a86bbf421b3594757adbfd273251719be778df30
rahulrayal <rahulrayal@dream11.com>
2023-07-30 10:07:40 +0530

    basic home page logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, useHistory } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle, settingsSharp } from 'ionicons/icons';
+import Tab1 from './pages/HomePage';
+import Tab2 from './pages/ContentPage';
+import Tab3 from './pages/Tab3';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+
+
+setupIonicReact({
+  mode: "ios",
+  });
 
 function App() {
+  let history = useHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonApp>
+   
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/tab1">
+            <Tab1 />
+          </Route>
+          <Route exact path="/tab2">
+            <Tab2 />
+          </Route>
+          <Route exact path="/tab3">
+            <Tab3 />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/tab1" />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/tab1">
+            <IonIcon aria-hidden="true" icon={triangle} />
+            <IonLabel>VaniTime</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/tab1">
+            <IonIcon aria-hidden="true" icon={ellipse} />
+            <IonLabel>History</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab1">
+            <IonIcon aria-hidden="true" icon={square} />
+            <IonLabel>Stats</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab1">
+            <IonIcon aria-hidden="true" icon={settingsSharp} />
+            <IonLabel>Settings</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+  </IonApp>
   );
 }
 
---
+++ src/pages/Tab3.js
@@ -0,0 +1,21 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+function Tab3(){
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Audio</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent >
+       <p><br /> </p><p><br /> </p> <div class="code">711205R3-DELHI - December 05, 1971 - 04:30 Minutes</div> <p><br />
+        <audio controls type="audio/mpeg" src="https://s3.amazonaws.com/vanipedia/full/1971/711205R3-DELHI.mp3"> Your browser does not support the audio element.</audio> 
+        </p>
+        <p><br /> Prabhupāda: . . . just like Arjuna said: "Yes." In the beginning he said: "No," then after learning <i>Bhagavad-gītā</i> he said: "Yes." So we have to learn to say: "Yes" to Kṛṣṇa or His representative. Then you will be all right. If you say: "No," then it is <i>māyā</i>. But you have got the liberty to say: "Yes" or "No." . . . (indistinct) . . . if we say: "No," then it is our misfortune. If we say: "Yes," it is our fortune. </p><p><i>Icchā-dveṣa.</i> What is that verse? <i>Sarge yānti parantapa</i>. You have got <i>icchā</i> and <i>dveṣa</i>—two things there. <i>Icchā</i> means either you have desire for sense gratification or you can desire to serve Kṛṣṇa. And <i>dveṣa</i> means averse. You may not like to serve Kṛṣṇa, or you may not like to serve <i>māyā</i>. </p> <dl><dd><i>icchā-dveṣa-samutthena</i></dd> <dd><i>sarge yānti parantapa</i></dd> <dd>(<a href="/wiki/BG_7.27_(1972)" title="BG 7.27 (1972)">BG 7.27</a>)</dd></dl> <p>So we have come here to this material world by two propensities, <i>icchā</i> and <i>dveṣa</i>. <i>Icchā</i>, we wanted to enjoy this material world; and <i>dveṣa</i>, we try to avoid Kṛṣṇa. You see? </p><p>(break) </p> <dl><dd><i>icchā-dveṣa-samutthena</i></dd> <dd><i>sarge yānti parantapa</i></dd></dl> <p>In this way you have come to this material world. </p><p>Just like this Pakistan and Hindustan, fighting. <i>Icchā</i>. <i>Icchā</i> is that "I shall enjoy this land." So they're fighting. They do not know that after all, this land does not belong to any one of us; it belongs to Kṛṣṇa. Why should we fight? Better you . . . let us be satisfied with whatever Kṛṣṇa has given. But there is no Kṛṣṇa consciousness; therefore they are fighting. Otherwise, Kṛṣṇa has made everything complete. </p> <dl><dd><i>pūrṇam idaṁ pūrṇam adaḥ</i></dd> <dd><i>pūrṇāt pūrṇam udacyate</i></dd> <dd>(<a href="/wiki/ISO_Invocation" title="ISO Invocation">Īśopaniṣad, Invocation</a>)</dd></dl> <p>This planet is complete because it is created by Kṛṣṇa. So there cannot be any scarcity, any want. Everything is there. But we are desiring. Such simple <i>dveṣa-samutthena</i>. <i>Dveṣa</i>. We want to enjoy more, therefore I envy you. This is our . . . (indistinct) . . . This is called material . . . (indistinct) </p><p>And we are being carried by this different types of desire, and we are getting different types of bodies. Kṛṣṇa is kind, Lord. Oh, whatever we desire—He supplies it, "All right." Like . . . but ultimately He says <i>sarva-guhyatamaṁ</i> (<a href="/wiki/BG_18.64_(1972)" title="BG 18.64 (1972)">BG 18.64</a>): "The most confidential part of knowledge I have given to you. Give up all this nonsense and surrender unto Me." So that is all. (cut) (end) </p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
