eebfd3e3f799a4a36255f3a27e9463e1cd7dfe6b
Susmit Kumar Mishra <50649721+susmitmishra125@users.noreply.github.com>
2023-08-05 03:10:46 +0530

    finding content logic

---
+++ src/pages/scripts/findRandomLecture.js
@@ -0,0 +1,78 @@
+import {minutesToMinutes, hoursToMinutes} from "./durationToMinutes.js";
+
+function generateLectures(lectureMap,vaniTime){
+	let lectures = [];
+	for(const topic in lectureMap){
+		if(lectureMap[topic]["checked"] != "false")
+		{
+			for(const lecture in lectureMap[topic]["parts"]){
+				{
+					if(!lecture["read"]){
+						let currTime = minutesToMinutes(lectureMap[topic]["parts"][lecture]["duration"]);
+						if (vaniTime-currTime >=0 && vaniTime-currTime<5) {
+							console.log(currTime, vaniTime, Math.abs(currTime-vaniTime)) 
+							lectures.push([lecture,lectureMap[topic]["parts"][lecture]["name"],currTime])
+						}
+					}
+				}
+			}
+		}
+	}
+	return lectures
+}
+
+function generateAllLectures(lectureMap){
+	let lectures = [];
+	for(const topic in lectureMap){
+		if(lectureMap[topic]["checked"] != "false")
+		{
+			for(const lecture in lectureMap[topic]["parts"]){
+				{
+					if(!lecture["read"]){
+						let currTime = minutesToMinutes(lectureMap[topic]["parts"][lecture]["duration"]);
+						lectures.push([lecture,lectureMap[topic]["parts"][lecture]["name"],currTime])
+					}
+				}
+			}
+		}
+	}
+	return lectures;
+}
+
+function binarySearch(arr, target) {
+	let left = 0;
+	let right = arr.length - 1;
+  
+	while (left <= right) {
+	  const mid = Math.floor((left + right) / 2);
+	  const value = arr[mid][2];
+  
+	  if (value === target) {
+		return arr[mid];
+	  } else if (value < target) {
+		left = mid + 1;
+	  } else {
+		right = mid - 1;
+	  }
+	}
+
+	if(arr[left][2] < target) return arr[left]
+	if(left != 0) return arr[left-1]
+	return arr[left]
+}
+
+
+const findRandomLecture = (lectureMap,vaniTime) => {
+	vaniTime = hoursToMinutes(vaniTime); 
+	let lectures = generateLectures(lectureMap,vaniTime);
+	if(lectures.length==0) {
+		lectures = generateAllLectures(lectureMap);
+		if(lectures.length == 0) return[]
+		lectures.sort((a, b) => a[2] - b[2]);
+		return binarySearch(lectures, vaniTime)
+	}
+	return lectures[Math.floor(Math.random() * lectures.length)]
+};
+
+
+export default findRandomLecture;
---
+++ src/pages/scripts/findRandomPurports.js
@@ -0,0 +1,90 @@
+import {hoursToMinutes} from "./durationToMinutes"; 
+
+function purportsList(booksData) {
+  let books = [];
+  if (booksData["BG"]["checked"] != "false") {
+    let purports = [];
+    for (const chapterIndex in booksData["BG"]["parts"]) {
+      if (booksData["BG"]["parts"][chapterIndex]["checked"] != "false") {
+        for (const verse in booksData["BG"]["parts"][chapterIndex]["parts"]) {
+          purports.push([
+            "BG_" + chapterIndex + "." + verse,
+            "Bhagavad Gita " + chapterIndex + "." + verse,
+            booksData["BG"]["parts"][chapterIndex]["parts"][verse]["words_count"],
+          ]);
+        }
+      }
+    }
+    books.push(purports);
+  } if (booksData["SB"]["checked"] != "false") {
+    let purports = [];
+    for (const cantoIndex in booksData["SB"]["parts"]) {
+      if (booksData["SB"]["parts"][cantoIndex]["checked"] != "false") {
+        for (const chapterIndex in booksData["SB"]["parts"][cantoIndex]["parts"]) {
+          if (booksData["SB"]["parts"][cantoIndex]["parts"][chapterIndex]["checked"] != "false") {
+            for (const verse in booksData["SB"]["parts"][cantoIndex]["parts"][chapterIndex]["parts"]) {
+              purports.push([
+                "SB_" + cantoIndex + "." + chapterIndex + "." + verse,
+                "Srimad Bhagavatam " + cantoIndex + "." + chapterIndex + "." + verse,
+                booksData["SB"]["parts"][cantoIndex]["parts"][chapterIndex]["parts"][verse]["words_count"],
+              ]);
+            }
+          }
+        }
+      }
+    }
+    books.push(purports);
+  } if (booksData["CC"]["checked"] != "false") {
+    let purports = [];
+    for (const lila in booksData["CC"]["parts"]) {
+      if (booksData["CC"]["parts"][lila]["checked"] != "false") {
+        for (const chapterIndex in booksData["CC"]["parts"][lila]["parts"]) {
+          if (booksData["CC"]["parts"][lila]["parts"][chapterIndex]["checked"] != "false") {
+            for (const verse in booksData["CC"]["parts"][lila]["parts"][chapterIndex]["parts"]) {
+              purports.push([
+                "CC_" + lila + "_" + chapterIndex + "." + verse,
+                "Caitanya Caritamrta " + lila + "." + chapterIndex + "." + verse,
+                booksData["CC"]["parts"][lila]["parts"][chapterIndex]["parts"][verse]["words_count"],
+              ]);
+            }
+          }
+        }
+      }
+    }
+    books.push(purports);
+  }
+  return books[Math.floor(Math.random() * books.length)];
+}
+
+function findRandomPurports(booksMap, vaniTime, wordsPerMin = 50) {
+  let counter = 0;
+  const purportList = purportsList(booksMap);
+	if(purportList.length==0) return []
+	vaniTime = hoursToMinutes(vaniTime);
+  let totalWords = wordsPerMin * vaniTime;
+  console.log(wordsPerMin, vaniTime, totalWords)
+  while (true) {
+    let start = Math.floor(Math.random() * purportList.length);
+    if (counter == 20) {
+      start = 0;
+    }
+    let wordsSumSoFar = purportList[start][2];
+    let purports = [];
+    purports.push(purportList[start])
+    start += 1;
+    while (wordsSumSoFar <= totalWords && start < purportList.length) {
+      if(wordsSumSoFar+purportList[start][2] > totalWords) break 
+      wordsSumSoFar += purportList[start][2];
+      purports.push(purportList[start]);
+      start += 1;
+    }
+    if ((wordsSumSoFar >= (totalWords - wordsPerMin*5) && wordsSumSoFar<=totalWords) || counter == 20) {
+			console.log(purports);
+      console.log("counter:", counter)
+      return purports;
+    }
+    counter++;
+  }
+}
+
+export default findRandomPurports;
